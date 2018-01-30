a21760042303a248751f44217a5843683b2f1782
nachshons <nachshons@wix.com>
2018-01-30 08:38:43 +0200

    Support skip option from inline webpack loader (regex sent as string)

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var utils = require('loader-utils');
-var cssWrap = require('css-wrap')
+var cssWrap = require('css-wrap');
 
 module.exports = function (source) {
-  this.cacheable();
+	this.cacheable();
 
-  var query = utils.parseQuery(this.query);
-  return cssWrap(source, query);
+	var query = utils.parseQuery(this.query);
+
+	fixSkipRegexOption(query);
+	return cssWrap(source, query);
 };
+
+function fixSkipRegexOption(options) {
+	if (options.skip) {
+		options.skip = (options.skip instanceof RegExp) ? options.skip : new RegExp(options.skip);
+	}
+}
